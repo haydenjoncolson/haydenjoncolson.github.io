bd14aeb234a6aac08b512c41980f36c7e6044c12
Hayden Jon <haydenjoncolson@gmail.com>
2016-12-05 23:15:37 -0800

    update

--- views/js/main.js
+++ views/js/main.js
@@ -423,8 +423,8 @@ var resizePizzas = function(size) {
 
    // Returns the size difference to change a pizza element from one size to another. Called by changePizzaSlices(size).
   function determineDx (elem, size) {
-    var oldWidth = elem.offsetWidth;
-    var windowWidth = document.querySelector("#randomPizzas").offsetWidth;
+    var oldWidth = elem.style.width;
+    var windowWidth = document.querySelector("#randomPizzas").style.width;
     var oldSize = oldWidth / windowWidth;
 
     // Changes the slider value to a percent width
