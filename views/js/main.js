6d212a7bf4900c36b09ee752dde40813c15bf296
Hayden Jon <haydenjoncolson@gmail.com>
2016-12-05 23:43:08 -0800

    update

--- views/js/main.js
+++ views/js/main.js
@@ -504,9 +504,8 @@ function updatePositions() {
   window.performance.mark("mark_start_frame");
 
   var items = document.querySelectorAll('.mover');
-  console.log('New call to updatePositions');
+  
   for (var i = 0; i < items.length; i++) {
-    console.log('i = ' + i);
     console.log("i % 5 = " + (i % 5));
     console.log("document.body.scrollTop = " + document.body.scrollTop);
     var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
